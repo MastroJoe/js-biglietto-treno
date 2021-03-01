6e061e983d841c4493d1e55f3b2b7d03873f2103
Giorgio <giorgio.pignattari@gmail.it>
2021-03-01 15:38:25 +0100

    inizio js output

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 // INPUT
+// età
+var eta = prompt("Inserisci la tua età:");
+// chilometri
+var chilometri = prompt("Inserisci i chilometri da percorrere:");
+
+console.log(parseInt(eta));
+console.log(parseFloat(chilometri));
+
+// OUTPUT
