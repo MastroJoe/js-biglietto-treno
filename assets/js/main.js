bf3eaf2f5099344ae86d70c4018bac5aecbed1b1
Giorgio <giorgio.pignattari@gmail.it>
2021-03-01 16:03:50 +0100

    calcolo dei biglietti

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,13 @@ console.log(parseInt(eta));
 console.log(parseFloat(chilometri));
 
 // OUTPUT
+// prezzo del biglietto
+var biglietto = chilometri * 0.21;
+// prezzo biglietto con sconto minorenni
+var minorenni = biglietto - (biglietto * 20 / 100);
+// prezzo biglietto con sconto over 65
+var over65 = biglietto - (biglietto * 40 / 100);
+
+console.log(biglietto);
+console.log(minorenni);
+console.log(over65);
